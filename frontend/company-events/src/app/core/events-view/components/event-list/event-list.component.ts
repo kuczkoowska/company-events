5164228df96f009b37414cb9c98f1fd13bca870a
Jagoda Kuczkowska <jagoda.kucz@wp.pl>
2025-06-01 23:38:08 +0200

    event list component add functionality

--- frontend/company-events/src/app/core/events-view/components/event-list/event-list.component.ts
+++ frontend/company-events/src/app/core/events-view/components/event-list/event-list.component.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
-import { MOCK_EVENTS } from '@company/shared/constants/mock-events.constant';
+import {Component, ResourceRef, inject, ResourceLoaderParams} from '@angular/core';
 import { IEvent } from '@company/shared/models/event.interface';
 import { CommonModule } from '@angular/common';
-import {EventDetailsComponent} from '@company/core/events-view/components/event-details/event-details.component';
+import { EventDetailsComponent } from '@company/core/events-view/components/event-details/event-details.component';
+import { EventsService } from '@company/core/services/events.service';
+import {rxResource} from '@angular/core/rxjs-interop';
 
 @Component({
-    selector: 'app-event-list',
-    templateUrl: './event-list.component.html',
-    styleUrls: ['./event-list.component.scss'],
+  selector: 'app-event-list',
+  templateUrl: './event-list.component.html',
+  styleUrls: ['./event-list.component.scss'],
   imports: [CommonModule, EventDetailsComponent],
+  standalone: true
 })
 export class EventListComponent {
-    events: IEvent[] = MOCK_EVENTS;
+  events: IEvent[] = [];
+  private eventsService: EventsService = inject(EventsService);
 
-    onDetails(): void {
+  public eventsResource: ResourceRef<IEvent[]> = rxResource({
+    loader: () => this.eventsService.getAllEvents()
+  })
 
-    }
+  public searchEventsByTagsResource: ResourceRef<IEvent[]> = rxResource({
+    loader: (params: ResourceLoaderParams<unknown>) => this.eventsService.searchEventsByTags(params as unknown as string)
+  })
+  onDetails(): void {
+
+  }
 }
