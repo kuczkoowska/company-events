d7b47e1ab26be5baff93483637c1caec7028acc1
Jagoda Kuczkowska <jagoda.kucz@wp.pl>
2025-06-02 02:02:59 +0200

    admin view style + functionality

--- frontend/company-events/src/app/core/admin-view/admin-view.component.ts
+++ frontend/company-events/src/app/core/admin-view/admin-view.component.ts
@@ -1,10 +1,47 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {Component, ResourceRef, inject, runInInjectionContext, Injector} from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import {EventsService} from '@company/core/services/events.service';
+import {IEvent} from '@company/shared/models/event.interface';
+import {rxResource} from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-admin-view',
-  imports: [],
-  template: './admin-view.component.html',
-  styleUrl: './admin-view.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './admin-view.component.html',
+  styleUrls: ['./admin-view.component.scss'],
+  standalone: true,
+  imports: [CommonModule, RouterModule]
 })
-export class AdminViewComponent { }
+export class AdminViewComponent {
+    activeTab: string = 'events';
+    private eventsService: EventsService = inject(EventsService);
+    private injector = inject(Injector);
+    filteredEvents: IEvent[] = [];
+
+  public eventsResource: ResourceRef<IEvent[]> = rxResource({
+    loader: () => this.eventsService.getAllEvents()
+  })
+
+  public filterEvents(filter: string) {
+    runInInjectionContext(this.injector, () => {
+      switch (filter) {
+        case 'upcoming':
+          this.eventsResource = rxResource({
+            loader: () => this.eventsService.getUpcomingEvents()
+          });
+          break;
+        case 'past':
+          this.eventsResource = rxResource({
+            loader: () => this.eventsService.getPastEvents()
+          });
+          break;
+        case 'ongoing':
+          this.eventsResource = rxResource({
+            loader: () => this.eventsService.getOnGoingEvents()
+          });
+          break;
+      }
+    });
+  }
+
+}
